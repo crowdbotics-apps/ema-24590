a7361d07d1a87e0bf8a44bb357ee8ea5d1bfce3e
Crowdbotics <team@crowdbotics.com>
2021-02-18 07:07:42 +0000

    Update admin title

--- ethermail_24590/urls.py
+++ ethermail_24590/urls.py
@@ -34,9 +34,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "EtherMail"
-admin.site.site_title = "EtherMail Admin Portal"
-admin.site.index_title = "EtherMail Admin"
+admin.site.site_header = "ema"
+admin.site.site_title = "ema Admin Portal"
+admin.site.index_title = "ema Admin"
 
 # swagger
 api_info = openapi.Info(
